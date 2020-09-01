0f7b9cfb55041e997836aeb343a024ccfc01d765
lili64140 <67411208+FEHLEN@users.noreply.github.com>
2020-09-01 13:06:57 +0200

    suppression de fichiers

--- view/teddy1.js
+++ view/teddy1.js
@@ -1,17 +1,57 @@
 
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  fetch("http://localhost:3000/api/teddies/5be9c8541c9d440000665243", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+
+// method get
+// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
+function ajaxGet(url, callback) {
+  var req = new XMLHttpRequest();
+  req.open("GET", url);
+  req.addEventListener("load", function () {
+      if (req.status >= 200 && req.status < 400) {
+          // Appelle la fonction callback en lui passant la réponse de la requête
+          callback(req.responseText);
+      } else {
+          console.error(req.status + " " + req.statusText + " " + url);
+      }
+  });
+  req.addEventListener("error", function () {
+      console.error("Erreur réseau avec l'URL " + url);
+  });
+  req.send(null);
+}
+
+//methode post
+function ajaxPost(url, options, callback) {
+  var req = new XMLHttpRequest();
+  req.open("POST", url);
+
+  var postOptions;
+  if (typeof options === 'objet'){
+    // znvoyer du json {plouf:lolo, name : ted ....}
+    req.setRequestHeader{'content-type','application/json'};
+
+  }else if (typeof options === "array"){
+
+  }else{
+    postOptions = null;
+  }
 
 
 
+  req.addEventListener("load", function () {
+      if (req.status >= 200 && req.status < 400) {
+          // Appelle la fonction callback en lui passant la réponse de la requête
+          callback(req.responseText);
+      } else {
+          console.error(req.status + " " + req.statusText + " " + url);
+      }
+  });
+  req.addEventListener("error", function () {
+      console.error("Erreur réseau avec l'URL " + url);
+  });
+  req.send(postOptions);
+}
 
-/*ajout du résultat dans la page*/
+function load
 
 
 
